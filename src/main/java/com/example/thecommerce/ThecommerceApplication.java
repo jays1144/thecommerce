045cfff3fa5362ce979cc1bce127632f7432e8fb
jay <daonj0715@gmail.com>
2024-04-24 13:11:45 +0900

    init

---
+++ src/main/java/com/example/thecommerce/ThecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.thecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThecommerceApplication.class, args);
+    }
+
+}
